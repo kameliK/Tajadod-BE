a4e2dff5949f9b37ab2ffcb7482c809e17a293c3
kamel qandalaft <qandalaftk@gmail.com>
2025-05-15 18:16:09 +0300

    new changes

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	    <title>مساعد إعادة التدوير الذكي</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="chat-container">
+    	            <h2>مساعد إعادة التدوير الذكي</h2>
+    	<div id="chat-box"></div>
+    	<div class="input-container">
+        <input type="text" id="user-input" placeholder="اكتب رسالتك هنا...">
+     <button onclick="sendMessage()">ارسل</button>
+    	</div>
+    </div>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
